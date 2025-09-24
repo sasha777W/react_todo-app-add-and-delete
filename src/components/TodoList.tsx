3e923c354630c7ee46f80fce8edff3080641c585
Oleksandr <lowka2018@gmail.com>
2025-09-24 17:39:56 +0200

    test

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -42,6 +42,7 @@ export const TodoList: React.FC<Props> = ({
             key={todo.id}
           >
             {/* eslint-disable jsx-a11y/label-has-associated-control */}
+            
             <label className="todo__status-label">
               <input
                 data-cy="TodoStatus"
